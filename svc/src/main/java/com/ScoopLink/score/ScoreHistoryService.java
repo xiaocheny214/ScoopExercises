facd69a31c15004bd0e728362fa0c2ab14948e0b
wuxinyu_ext <wuxy16@asiainfo.com>
2026-01-14 15:36:32 +0800

    修复找不到spring容器找不到bean的问题；修复用户重复答题，无法提交试卷的问题

--- svc/src/main/java/com/ScoopLink/score/ScoreHistoryService.java
+++ svc/src/main/java/com/ScoopLink/score/ScoreHistoryService.java
@@ -19,11 +19,13 @@ import com.ScoopLink.scoreCalculation.server.StatisticsScore;
 import com.ScoopLink.userAnswers.dto.UserAnswer;
 import com.ScoopLink.userAnswers.server.UserAnswersServer;
 import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@Service
 public class ScoreHistoryService implements StatisticsScore {
 
     @Resource
